5cfb34924d547c02ebb876c48690413b145187d9
anthony ervin <ajervin1@asu.edu>
2021-03-02 13:57:50 -0700

    change baseurl

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 
 // Get User Posts
 export async function fetchUserPosts ({ username = '', media_type = 'image', end_cursor = null }) {
-	const { data } = await axios.get('http://localhost:4000/search', {
+	const { data } = await axios.get('https://instaserver100.herokuapp.com//search', {
 		params: {
 			username,
 			media_type,
